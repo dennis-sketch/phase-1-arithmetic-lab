5e76a3a21de3f9ad2d379c4797a56b6d72c4d45e
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-10-12 20:24:30 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 // Write your code here
+// Task 1
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Task 2
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Task 3
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Task 4
+const max = Math.max(10, 20, 15);
+
+// Printing the results
+console.log('Task 1 - Multiply:', multiply);
+console.log('Task 2 - Random:', random);
+console.log('Task 3 - Mod:', mod);
+console.log('Task 4 - Max:', max);
+
+
